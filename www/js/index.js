98e46a5695c9d64856eaf54b3c19c7bdac568ea6
ReinaldoDomingos <reijunior100@gmail.com>
2019-10-17 15:24:30 -0400

    Verificar conexão e abrir app de Triagem

---
+++ www/js/index.js
@@ -0,0 +1,14 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function() {
+        iniciar()
+    },
+
+    // Update DOM on a Received Event
+};
+
+app.initialize();
